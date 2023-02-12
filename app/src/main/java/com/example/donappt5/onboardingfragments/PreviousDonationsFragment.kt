b8dbbc7d61d619df4ed81eb29f36e83d87a153d0
fukenrice <63079551+fukenrice@users.noreply.github.com>
2023-02-12 15:37:31 +0300

    Add onboarding

---
+++ app/src/main/java/com/example/donappt5/onboardingfragments/PreviousDonationsFragment.kt
@@ -0,0 +1,132 @@
+package com.example.donappt5.onboardingfragments
+
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TableLayout
+import android.widget.TableRow
+import androidx.fragment.app.Fragment
+import com.example.donappt5.R
+import com.example.donappt5.databinding.FragmentPreviousDonationsBinding
+import com.example.donappt5.helpclasses.OnBoardingDonationRecord
+import kotlinx.android.synthetic.main.fragment_previous_donations.view.*
+import kotlinx.android.synthetic.main.onboarding_table_row.view.*
+
+private const val PREVIOUS_DATA = "prevData"
+private const val GUIDE = "guide"
+
+class PreviousDonationsFragment : Fragment() {
+    private var prevData: ArrayList<OnBoardingDonationRecord>? = null
+    private var guide: String? = null
+    private lateinit var binding: FragmentPreviousDonationsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            prevData = it.getSerializable(PREVIOUS_DATA) as ArrayList<OnBoardingDonationRecord>?
+            guide = it.getString(GUIDE)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentPreviousDonationsBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onResume() {
+        super.onResume()
+        setupView()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        prevData = arrayListOf()
+        for (i in 0 until binding.tlOnboarding.childCount) {
+            val row = binding.tlOnboarding.getChildAt(i) as TableRow
+            val etCharityName = row.getChildAt(0) as EditText
+            val etDonation = row.getChildAt(1) as EditText
+            if (etCharityName.text.toString() != "") {
+                prevData!!.add(OnBoardingDonationRecord(etCharityName.text.toString(), etDonation.text.toString().toIntOrNull()))
+            }
+        }
+    }
+
+    private fun setupView() {
+        binding.apply {
+            btnAddRow.setOnClickListener {
+                addRow()
+            }
+            tvGuide.text = guide
+            binding.tlOnboarding.removeAllViews()
+        }
+
+        if (prevData != null) {
+            for (i in prevData!!) {
+                addRow(i.charityName, i.monthlyDonation)
+            }
+        }
+    }
+
+    public fun addRow(name: String = "", donation: Int? = null) {
+        val inflater = LayoutInflater.from(context)
+        val tr = inflater.inflate(R.layout.onboarding_table_row, null) as TableRow
+        val etName = tr.findViewById<EditText>(R.id.etCharityName)
+        etName.setText(name)
+        val etDonation  = tr.findViewById<EditText>(R.id.etDonation)
+        if (donation != null) {
+            etDonation.setText(Integer.toString(donation))
+        }
+        tr.btnDeleteRow.setOnClickListener {
+            AlertDialog.Builder(context)
+                .setTitle("Delete record")
+                .setMessage("Do you really want to delete thish record?")
+                .setPositiveButton("yes", DialogInterface.OnClickListener { dialog, which ->
+                    prevData!!.removeAt(binding.tlOnboarding.indexOfChild(tr))
+                    binding.tlOnboarding.removeView(tr)
+                    binding.tlOnboarding.invalidate()
+                })
+                .setNegativeButton("no", null)
+                .show()
+        }
+        binding.tlOnboarding.addView(tr) //добавляем созданную строку в таблицу
+    }
+
+
+    public fun addData(data: ArrayList<OnBoardingDonationRecord>) {
+        for (i in data) {
+            addRow(i.charityName, i.monthlyDonation)
+        }
+    }
+
+    public fun getData(): MutableList<OnBoardingDonationRecord> {
+        val res = mutableListOf<OnBoardingDonationRecord>()
+        for (i in 0 until binding.tlOnboarding.childCount) {
+            val row = binding.tlOnboarding.getChildAt(i) as TableRow
+            val etCharityName = row.getChildAt(0) as EditText
+            val etDonation = row.getChildAt(1) as EditText
+            if (etCharityName.text.toString() != "") {
+                res.add(OnBoardingDonationRecord(etCharityName.text.toString(), etDonation.text.toString().toIntOrNull()))
+            }
+        }
+        return res
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(guide: String, prevData: ArrayList<OnBoardingDonationRecord>? = null) =
+            PreviousDonationsFragment().apply {
+                arguments = Bundle().apply {
+                    putString(GUIDE, guide)
+                    putSerializable(PREVIOUS_DATA, prevData)
+                }
+            }
+    }
+}
---
+++ app/src/main/java/com/example/donappt5/onboardingfragments/TagsFragment.kt
@@ -0,0 +1,130 @@
+package com.example.donappt5.onboardingfragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.View.GONE
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.donappt5.databinding.PopupCheckboxesBinding
+import kotlinx.android.synthetic.main.popup_checkboxes.*
+import kotlinx.android.synthetic.main.popup_checkboxes.view.*
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ART = "art"
+private const val KIDS = "kids"
+private const val POVERTY = "poverty"
+private const val SCIENCE_AND_RESEARCH = "science&research"
+private const val HEALTHCARE = "healthcare"
+private const val EDUCATION = "education"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [TagsFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class TagsFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private lateinit var binding: PopupCheckboxesBinding
+    private val state = mutableMapOf(
+        "art" to false,
+        "kids" to false,
+        "poverty" to false,
+        "science&research" to false,
+        "healthcare" to false,
+        "education" to false
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            state[ART] = it.getBoolean(ART)
+            state[KIDS] = it.getBoolean(KIDS)
+            state[POVERTY] = it.getBoolean(POVERTY)
+            state[SCIENCE_AND_RESEARCH] = it.getBoolean(SCIENCE_AND_RESEARCH)
+            state[HEALTHCARE] = it.getBoolean(HEALTHCARE)
+            state[EDUCATION] = it.getBoolean(EDUCATION)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = PopupCheckboxesBinding.inflate(inflater, container, false)
+        val view = binding.root
+        setupView(view)
+        return view
+    }
+
+    private fun setupView(view: View) {
+        view.apply {
+            btnSetTags.setVisibility(GONE)
+
+            kidscb.isChecked = state[KIDS] == true
+            povcb.isChecked = state[POVERTY] == true
+            rescb.isChecked = state[SCIENCE_AND_RESEARCH] == true
+            artcb.isChecked = state[ART] == true
+            helcb.isChecked = state[HEALTHCARE] == true
+            educb.isChecked = state[EDUCATION] == true
+        }
+    }
+
+    public fun getData(): Map<String, Boolean> = mapOf(
+        KIDS to binding.kidscb.isChecked,
+        POVERTY to binding.povcb.isChecked,
+        SCIENCE_AND_RESEARCH to binding.rescb.isChecked,
+        ART to binding.artcb.isChecked,
+        HEALTHCARE to binding.helcb.isChecked,
+        EDUCATION to binding.educb.isChecked
+    )
+
+    public fun addData(
+        art: Boolean?,
+        kids: Boolean?,
+        poverty: Boolean?,
+        scienceAndResearch: Boolean?,
+        healthcare: Boolean?,
+        education: Boolean?
+    ) {
+        kidscb.isChecked = kids == true
+        povcb.isChecked = poverty == true
+        rescb.isChecked = scienceAndResearch == true
+        artcb.isChecked = art == true
+        helcb.isChecked = healthcare == true
+        educb.isChecked = education == true
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment TagsFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(
+            art: Boolean = false,
+            kids: Boolean = false,
+            poverty: Boolean = false,
+            scienceAndResearch: Boolean = false,
+            healthcare: Boolean = false,
+            education: Boolean = false
+        ) =
+            TagsFragment().apply {
+                arguments = Bundle().apply {
+                    putBoolean(ART, art)
+                    putBoolean(KIDS, kids)
+                    putBoolean(POVERTY, poverty)
+                    putBoolean(SCIENCE_AND_RESEARCH, scienceAndResearch)
+                    putBoolean(HEALTHCARE, healthcare)
+                    putBoolean(EDUCATION, education)
+                }
+            }
+    }
+}
